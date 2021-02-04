08a90de7067ee3536c19c97e3b5427f2c64156b4
StratosphereXE <73959824+StratosphereXE@users.noreply.github.com>
2021-02-04 23:01:21 +0530

    Add files via upload

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,26 +1,69 @@
-var gamePattern=[];
-var userClickedPattern=[];
+var gamePattern = [];
+var userClickedPattern = [];
+var level = 0;
+var buttonColor = ["red", "blue", "green", "yellow"];
 
-var buttonColor= ["red","blue","green","yellow"];
+//function to animate user pressed button
+function animatePress(currentColor) {
+  $('#' + currentColor)
+  .addClass('pressed')
+  .delay(1000)
+  .queue(function(next){
+    $(this).removeClass('pressed');
+    next();
+  });
+}
 
-function nextSequence(num){
-  var randomNumber = Math.floor(Math.random()*(4-0));
-  return randomNumber;
+//function to play sound of button
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function checkAnswer(currentLevel){
+  if(gamePattern[currentLevel]==userClickedPattern[currentLevel])
+  {
+    console.log("success");
+    if(userClickedPattern.length==gamePattern.length)
+    {
+      setTimeout(function () {
+        level++;
+        nextSequence();
+      }, 2000);
+    }
+
+  }
+    else{
+      console.log("fail");
+    }
 
 }
 
-var randomChosenColor = buttonColor[nextSequence()];
+//function to chosen random button
+function nextSequence() {
+  userClickedPattern=[];
+  var randomNumber = Math.floor(Math.random() * (4 - 0));
+  var randomChosenColor = buttonColor[randomNumber];
 
-gamePattern.push(randomChosenColor);
+  gamePattern.push(randomChosenColor);
+  $('#' + randomChosenColor).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColor);
+  $("h1").text("Level " + level);
 
-$('#' + randomChosenColor).fadeOut(100).fadeIn(100);
-var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
-audio.play();
+}
 
-$(".btn").on('click', function(){
-  var userChosenColor= (this.id);
-  userClickedPattern = userChosenColor;
+$(".btn").on('click', function() {
+  var userChosenColor = $(this).attr('id');
+  userClickedPattern.push(userChosenColor);
+  playSound(userChosenColor);
+  animatePress(userChosenColor);
+  checkAnswer(level);
 
 });
 
-console.log(userClickedPattern);
+if(level==0){
+$(document).keydown(function() {
+  nextSequence();
+
+});
+}
